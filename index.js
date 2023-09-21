3ac7caf781537c6cd29a39c73e2a5bd4a1cf7372
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-09-21 16:43:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 let buttons = document.querySelectorAll(".block-menu");
 
-buttons.forEach(function(button) {
-    let blockQuestionMenu = button.querySelector(".block-question-menu"); 
+buttons.forEach(function (button) {
+  let blockQuestionMenu = button.querySelector(".block-question-menu");
+
+  blockQuestionMenu.style.display = "none";
+
+  button.addEventListener("click", function () {
+    if (blockQuestionMenu.style.display === "none") {
+      blockQuestionMenu.style.display = "block";
+    } else {
+      blockQuestionMenu.style.display = "none";
+    }
+  });
+});
+
+var openBtn = document.querySelector(".open-btn");
+var closeBtn = document.querySelector(".close-btn");
+var activeBlock = document.querySelector(".block__button-open_active");
 
 
-    blockQuestionMenu.style.display = 'none';
+function showBlock() {
+  activeBlock.style.display = "block";
+}
 
-    button.addEventListener("click", function() {
-        if (blockQuestionMenu.style.display === "none") {
-            blockQuestionMenu.style.display = "block";
-        } else {
-            blockQuestionMenu.style.display = "none";
-        }
-    });
+
+function hideBlock() {
+  activeBlock.style.display = "none";
+}
+
+
+openBtn.addEventListener("click", showBlock);
+closeBtn.addEventListener("click", hideBlock);
+
+const menuIcone = document.querySelector(".menu-icon"),
+  header = document.querySelector("header");
+
+menuIcone.addEventListener("click", () => {
+  menuIcone.classList.toggle("menu-icon-active");
+  header.classList.toggle("header__mobile");
+});
+
+document.querySelector(".menu-icon").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(".menu").classList.toggle("menu-active");
 });
